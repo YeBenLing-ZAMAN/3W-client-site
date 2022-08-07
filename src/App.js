4b42a08ba692a4f2ca7a6cce2ce9e82ffea817fa
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-08-07 23:15:15 +0600

    signup section added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './Component/Authencation/Login';
+import SignUp from './Component/Authencation/SignUp';
+import Navbar from './Component/Header.js/Navbar';
 
 function App() {
   return (
     <>
-
-      <Login></Login>
+    <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+      </Routes>
     </>
   );
 }
---
+++ src/Component/Header.js/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <div>
+            {/* navbar */}
+            <div className="bg-base-300">
+                <div className='navbar max-w-7xl mx-auto'>
+                    <div className="flex-1">
+                        <p className="normal-case text-xl" >Logo</p>
+                    </div>
+                    <div className="flex-none">
+                        <p className="font-bold">User Name<span></span> </p>
+
+                        <div className="dropdown dropdown-end mt-1">
+                            <label tabIndex="0" className="">
+                                <div className="avatar mx-2">
+                                    <div className="w-10 rounded-full ring ring-offset-base-100 ring-offset-2">
+                                        <img src="https://source.unsplash.com/mEZ3PoFGs_k" alt=''/>
+                                    </div>
+                                </div>
+                            </label>
+                            <ul tabIndex="0" className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                                <li><Link to='/dashboard/profile'>My Profile</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Navbar;
