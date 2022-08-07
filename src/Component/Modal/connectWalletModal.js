1edaee41c8858041c2c97524b3ffeb57c4862658
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-08-08 05:00:18 +0600

    modal connected

--- src/Component/Modal/connectWalletModal.js
+++ src/Component/Modal/connectWalletModal.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import metaMarkImage from '../../assets/metamask.png';
+import euroImage from '../../assets/wallet.png';
 
 const connectWalletModal = () => {
     return (
@@ -8,8 +10,15 @@ const connectWalletModal = () => {
                 <div className="modal-box ">
                     <label htmlFor="connectWalletModal-popup" className="btn btn-sm btn-circle absolute right-3">✕</label>
                     <h3 className="text-lg font-bold">Connect Your Wallet!</h3>
-                    <div>
-
+                    <div className='flex flex-col lg:flex-row items-center justify-around mt-4'>
+                        <div className='flex flex-col justify-center p-5 hover:cursor-pointer'>
+                            <img src={metaMarkImage} alt="" />
+                            <h3 className='text-primary'>MetaMask</h3>
+                        </div>
+                        <div className='flex flex-col justify-center p-5  hover:cursor-pointer'>
+                            <img src={euroImage} alt="" />
+                            <h3 className='text-primary'>WalletConnect</h3>
+                        </div>
                     </div>
                 </div>
             </div>
